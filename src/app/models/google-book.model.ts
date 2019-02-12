4e2b4ccfc79ad6d94a6672b8d69a78404b3c4df2
laurent <laurent.picard.LP2@gmail.com>
2019-02-12 12:20:09 +0100

    datamock googlebooks

---
+++ src/app/models/google-book.model.ts
@@ -0,0 +1,23 @@
+import { Author } from './auhtor.model';
+
+export class GoogleBook {
+    public bookId: number;
+ 
+    constructor (
+        public availableQuantity: number,
+        public categorie: string,
+        public codeISBN: string,
+        public description: string,
+        public imgThumbnail: string,
+        public isEbook: boolean,
+        public langage: string,
+        public pageCount: number,
+        public price: number,
+        public publishReleased: string,
+        public textSnippet: string,
+        public title: string,
+        public gendleId: number,
+        public publishedId: number,
+        public authors: Author
+        ) {}
+}
